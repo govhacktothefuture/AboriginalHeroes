9aa191f4e58babee3c341c6e74c6317e12717c89
Benjamin Collett <govhacktothefuture@gmail.com>
2015-07-04 09:51:19 +0800

    Added datasource object in data project

---
+++ AboriginalHeroes.Entities/DataGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboriginalHeroes.Entities
+{
+    public class DataGroup
+    {
+        public DataGroup(String uniqueId, String title, String subtitle, String imagePath, String description)
+        {
+            this.UniqueId = uniqueId;
+            this.Title = title;
+            this.Subtitle = subtitle;
+            this.Description = description;
+            this.ImagePath = imagePath;
+            this.Items = new ObservableCollection<DataItem>();
+        }
+
+        public string UniqueId { get; private set; }
+        public string Title { get; private set; }
+        public string Subtitle { get; private set; }
+        public string Description { get; private set; }
+        public string ImagePath { get; private set; }
+        public ObservableCollection<DataItem> Items { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
---
+++ AboriginalHeroes.Entities/DataItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboriginalHeroes.Entities
+{
+    public class DataItem
+    {
+        public DataItem(String uniqueId, String title, String subtitle, String imagePath, String description, String content)
+        {
+            this.UniqueId = uniqueId;
+            this.Title = title;
+            this.Subtitle = subtitle;
+            this.Description = description;
+            this.ImagePath = imagePath;
+            this.Content = content;
+        }
+
+        public string UniqueId { get; private set; }
+        public string Title { get; private set; }
+        public string Subtitle { get; private set; }
+        public string Description { get; private set; }
+        public string ImagePath { get; private set; }
+        public string Content { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
