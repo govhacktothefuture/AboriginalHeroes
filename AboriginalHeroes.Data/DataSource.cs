0a48a1e1007c7b34508860d947ba2ee62cb2a0bf
Benjamin Collett <govhacktothefuture@gmail.com>
2015-07-04 10:07:20 +0800

    Added missing files to solution

---
+++ AboriginalHeroes.Data/DataSource.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AboriginalHeroes.Entities;
+using Windows.Storage;
+using Windows.Data.Json;
+
+namespace AboriginalHeroes.Data
+{
+    public sealed class DataSource
+    {
+        private static DataSource _dataSource = new DataSource();
+
+        private ObservableCollection<DataGroup> _groups = new ObservableCollection<DataGroup>();
+        public ObservableCollection<DataGroup> Groups
+        {
+            get { return this._groups; }
+        }
+
+        public static async Task<IEnumerable<DataGroup>> GetGroupsAsync()
+        {
+            await _dataSource.GetDataAsync();
+
+            return _dataSource.Groups;
+        }
+
+        public static async Task<DataGroup> GetGroupAsync(string uniqueId)
+        {
+            await _dataSource.GetDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _dataSource.Groups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static async Task<DataItem> GetItemAsync(string uniqueId)
+        {
+            await _dataSource.GetDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _dataSource.Groups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        private async Task GetDataAsync()
+        {
+            if (this._groups.Count != 0)
+                return;
+
+            Uri dataUri = new Uri("ms-appx:///DataModel/SampleData.json");
+
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(dataUri);
+            string jsonText = await FileIO.ReadTextAsync(file);
+            JsonObject jsonObject = JsonObject.Parse(jsonText);
+            JsonArray jsonArray = jsonObject["Groups"].GetArray();
+
+            foreach (JsonValue groupValue in jsonArray)
+            {
+                JsonObject groupObject = groupValue.GetObject();
+                DataGroup group = new DataGroup(groupObject["UniqueId"].GetString(),
+                                                            groupObject["Title"].GetString(),
+                                                            groupObject["Subtitle"].GetString(),
+                                                            groupObject["ImagePath"].GetString(),
+                                                            groupObject["Description"].GetString());
+
+                foreach (JsonValue itemValue in groupObject["Items"].GetArray())
+                {
+                    JsonObject itemObject = itemValue.GetObject();
+                    group.Items.Add(new DataItem(itemObject["UniqueId"].GetString(),
+                                                       itemObject["Title"].GetString(),
+                                                       itemObject["Subtitle"].GetString(),
+                                                       itemObject["ImagePath"].GetString(),
+                                                       itemObject["Description"].GetString(),
+                                                       itemObject["Content"].GetString()));
+                }
+                this.Groups.Add(group);
+            }
+        }
+    }
+}
