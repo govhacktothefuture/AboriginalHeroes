8f6e5aa7da6584764123df8a34cd279538d444b0
MengSan <mh-chua@hotmail.com>
2015-07-04 19:11:03 +0800

    Added Group4 Data.

---
+++ AboriginalHeroes.Data/DataModels/daa/daa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboriginalHeroes.Data.DataModels.Daa
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public string rank { get; set; }
+        public string placeOfBirth { get; set; }
+        public string died { get; set; }
+        public string serviceDate { get; set; }
+        public string photo { get; set; }
+        public string article { get; set; }
+    }
+
+    public class Group
+    {
+        public string UniqueId { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public List<Item> Items { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Group> Groups { get; set; }
+    }
+}
