efd607656d33f2fe8319fe2088a3bbcfe901de6e
Benjamin Collett <govhacktothefuture@gmail.com>
2015-07-04 09:37:43 +0800

    Added dataservice test.

--- AboriginalHeroes.Data/DataService.cs
+++ AboriginalHeroes.Data/DataService.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 namespace AboriginalHeroes.Data
 {
     public class DataService
-    
+    {
+        /// <summary>
+        /// url =  http://(urlHere)
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<string> GetJsonStream(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            string content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
 
     }
+
 }
---
+++ AboriginalHeroes.DataTests/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using AboriginalHeroes.Data;
+using System.Threading.Tasks;
+
+namespace AboriginalHeroes.DataTests
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void DownloadJsonTest()
+        {
+            var service = new DataService();
+            Task<string> task = service.GetJsonStream(@"https://www.awm.gov.au/direct/data.php?key=WW1HACK2015&q=aboriginal&start=40&count=20");
+            task.Wait();
+            Assert.IsNotNull(task.Result);
+        }
+    }
+}
